a7537496fb9142992b4992fbbc3249178ee89e42
Davi Trajano <davitrajano473@gmail.com>
2022-11-16 23:26:45 -0300

    Primeiro Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function botao(){
+   var y = document.querySelector("body");
+    if(y.bgColor==""){
+        y.setAttribute("bgColor","grey");
+        }else{
+            y.setAttribute("bgColor","");
+        }
+}

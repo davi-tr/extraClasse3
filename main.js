179fdf9302ed76c45985798a234acde484a3599b
Davi Trajano <davitrajano473@gmail.com>
2022-11-17 00:36:28 -0300

    Merge Fixed2

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 function botao(){
    var y = document.querySelector("body");
     if(y.bgColor==""){
-<<<<<<< HEAD
+        
         y.setAttribute("bgColor","orange");
-=======
-        y.setAttribute("bgColor","green");
->>>>>>> aef4b7460906dfa2fca966bc83d1ed1bf37172a2
+
         }else{
             y.setAttribute("bgColor","");
         }
